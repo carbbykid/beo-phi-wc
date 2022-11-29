817d508000df882bc3c2041f24b932f4d5c5f6d5
chipopo <carbbykid@gmail.com>
2022-11-29 15:12:00 +0700

    fix: test done

--- src/pages/api/worldcup.ts
+++ src/pages/api/worldcup.ts
@@ -1,33 +1,33 @@
-// import ResultMatch from "models/ResultMatch";
+import ResultMatch from "models/ResultMatch";
 import { NextApiRequest, NextApiResponse } from "next";
-// import dbConnect from "../../util/mongodb";
+import dbConnect from "../../util/mongodb";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const { method, body } = req;
-  // dbConnect();
+  dbConnect();
   if (method === "GET") {
     try {
       res.status(200).json({ hello: "helloo" });
-      // const resultsMatch = await ResultMatch.find();
+      const resultsMatch = await ResultMatch.find();
       // res.status(200).json(resultsMatch);
     } catch (error) {
       res.status(500).json(error);
     }
   }
-  // if (method === "POST") {
-  //   try {
-  //     const haveBattle = await ResultMatch.find({ id: body.id });
-  //     if (haveBattle.length !== 0) {
-  //       res.status(500).json({ message: "Đã tạo rồi nhé!" });
-  //       return;
-  //     } else {
-  //       const resultMatch = await ResultMatch.create(body);
-  //       res.status(201).json(resultMatch);
-  //     }
-  //   } catch (error) {
-  //     res.status(500).json(error);
-  //   }
-  // }
+  if (method === "POST") {
+    try {
+      const haveBattle = await ResultMatch.find({ id: body.id });
+      if (haveBattle.length !== 0) {
+        res.status(500).json({ message: "Đã tạo rồi nhé!" });
+        return;
+      } else {
+        const resultMatch = await ResultMatch.create(body);
+        res.status(201).json(resultMatch);
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
 }
